73d26e06418228e35a944606a8790b2fbc40f3d0
Ariadna Sophia Cabrera - Entry Level Developer <scabrera@indigo.tech>
2025-04-29 21:50:32 -0500

    Primer commit payroll proyect

---
+++ payroll_proyect/src/main/java/co/edu/unbosque/db2/payroll_proyect/PayrollProyectApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.db2.payroll_proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollProyectApplication.class, args);
+	}
+
+}
